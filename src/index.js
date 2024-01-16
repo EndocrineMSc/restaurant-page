ec22986e07341c2a9697a429f4c2984a889f871e
EndocrineMSc <bamsc.studio@gmail.com>
2024-01-16 17:16:13 +0100

    Add active class to button corresponding with active content, for navigation styling

--- src/index.js
+++ src/index.js
@@ -2,17 +2,31 @@ import "./style.css";
 import { landingPage } from "./landingPage";
 import { foodPage } from "./foodPage";
 
+function toggleActiveButton(event) {
+    event.target.classList.add("active");
+
+    let buttons = Array.from(sideBar.getElementsByTagName("button"));
+    buttons.forEach(button => {
+        if (event.target != button) {
+            button.classList.remove("active");
+        }
+    });
+}
+
 const sideBar = document.getElementById("side-bar");
 
 const homeButton = document.createElement("button");
 homeButton.id = "home-button";
+homeButton.classList.add("active");
 homeButton.textContent = "Home";
 homeButton.addEventListener("click", landingPage.render);
+homeButton.addEventListener("click", toggleActiveButton);
 
 const foodButton = document.createElement("button");
 foodButton.id = "food-button";
 foodButton.textContent = "Food";
 foodButton.addEventListener("click", foodPage.render);
+foodButton.addEventListener("click", toggleActiveButton);
 
 const drinksButton = document.createElement("button");
 drinksButton.id = "drinks-button";
